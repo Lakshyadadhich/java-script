4bcce64dda8e4635484c766a4a0ef1a315ebc1cb
lakshya dadhich <90245926+Lakshyadadhich@users.noreply.github.com>
2024-02-27 06:00:35 +0000

    in this we learn about data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) -> if we use this in the brower's console or with html this will show us a alert pop-up but here we are using nodejs, not the browser so here the way of using this is differnet
+
+
+//  number => 2 to power 53
+//  bigint
+//  string =>"sdsd"
+//  boolean => true/fase
+//  null => standalone value
+//  undefined
+//  objects
+
+console.log(typeof null); //this will give us output as "object"
+
+console.log(typeof undefined); //this will give us undefined as a output 
