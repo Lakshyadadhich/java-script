d031177964599cfdffcbc54c3d5d6d56e25a527f
lakshya dadhich <90245926+Lakshyadadhich@users.noreply.github.com>
2024-02-27 05:31:46 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 1234
+/* const is use where you don't want to change the value in futue  */
+var accountEmail = "lakshya@gmail.com"
+/* var is the variable type in which we can change the value but don't use the var because var have issue that is block scope and functional scope.*/
+let accountPassword = " 1212121"
+/* let is also a variable type in which there is no issue like var and rest of the same in this like var*/
+
+
+console.table([accountEmail,accountID,accountPassword])
+
