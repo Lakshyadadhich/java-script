ac0c8a2b2dd0a0ae4d50a2f85860e138d611a627
lakshya dadhich <90245926+Lakshyadadhich@users.noreply.github.com>
2024-02-28 05:08:56 +0000

    data conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+// when we convert any data type into number then we find
+
+let scroe = 33;
+console.log(typeof scroe);
+
+/* 
+like this 33 => number 33
+        "33" =>NaN
+        true=> 1, false =>0
+*/
+
+let data = 1
+console.log(Boolean(data));
+
+/* 0 => true
+    1 => false
+    "" =>false
+    "lakshya" => ture */
