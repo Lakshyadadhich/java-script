f6e71cd7b208371d628b07fd081959f3a203f50e
lakshya dadhich <90245926+Lakshyadadhich@users.noreply.github.com>
2024-03-02 17:35:43 +0000

    done the dates topic

---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+/* js me array baki languages se alg hote hain 
+isme hum ek hi variable me different types ke data ko store kr skte hain */
+
+let arr = [1,2,3,'lakshya'];
+
+console.log(arr[3]);
+
+/* when we use copy operation in js it create the copies in shallow copies 
+
+==> shallow copy me jo copy hoti hain uska reference same hota hain origianl data se*/
